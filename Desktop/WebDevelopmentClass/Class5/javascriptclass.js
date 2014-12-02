d95afaaa2fb8b296a85f5ed5d6315dca6aa1c486
kateengel <katelynengel@gmail.com>
2014-12-02 18:58:10 -0500

    commit class 4

---
+++ Desktop/WebDevelopmentClass/Class5/javascriptclass.js
@@ -0,0 +1,7 @@
+alert('Hello World!');
+
+function change() {
+	document.getElementById('el').innerHTML = "Hello World!!"
+}
+// This allows you to manipulate the existing element by overwriting the words with Hello World 
+
